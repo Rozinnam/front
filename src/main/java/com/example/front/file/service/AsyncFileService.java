64d1da4bb9963562b59ae21d2918da8b1f28f894
Damho Lee <ydh4745@gmail.com>
2025-04-07 17:51:34 +0900

    [feat] FileService.java 인터페이스 생성, 이를 구현하는 구현체 AsyncFileService.java, SyncFileService.java 생성. @ConditionalOnProperty 로 관리

---
+++ src/main/java/com/example/front/file/service/AsyncFileService.java
@@ -0,0 +1,45 @@
+package com.example.front.file.service;
+
+import com.example.front.file.adaptor.AsyncFileAdaptor;
+import com.example.front.file.exception.FileEmptyException;
+import com.example.front.file.exception.FileUnsupportedFormatException;
+import com.example.front.file.utils.FileUtils;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+import java.util.UUID;
+
+@Slf4j
+@Service
+@ConditionalOnProperty(name = "app.mode", havingValue = "async")
+@RequiredArgsConstructor
+public class AsyncFileService implements FileService {
+    private final AsyncFileAdaptor fileAdaptor;
+    private final FileUtils fileUtils;
+
+    @Override
+    public String communicateWithAiServer(List<MultipartFile> files) {
+        if (files == null || files.isEmpty()) {
+            throw new FileEmptyException();
+        }
+
+        for (MultipartFile file : files) {
+            if (!fileUtils.isValidMimeType(file)) {
+                throw new FileUnsupportedFormatException();
+            }
+        }
+
+        String taskId = generateTaskId();
+        fileAdaptor.communicateWithAiServer(files, taskId);
+
+        return taskId;
+    }
+
+    private String generateTaskId() {
+        return UUID.randomUUID().toString();
+    }
+}
--- src/main/java/com/example/front/file/service/FileService.java
+++ src/main/java/com/example/front/file/service/FileService.java
@@ -1,36 +1,9 @@
 package com.example.front.file.service;
 
-import com.example.front.file.adaptor.FileAdaptor;
-import com.example.front.file.exception.FileEmptyException;
-import com.example.front.file.exception.FileUnsupportedFormatException;
-import com.example.front.file.utils.FileUtils;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+
 import java.util.List;
 
-@Slf4j
-@Service
-@RequiredArgsConstructor
-public class FileService {
-    private final FileAdaptor fileAdaptor;
-    private final FileUtils fileUtils;
-
-    public String communicateWithAiServer(List<MultipartFile> files) {
-        if (files == null || files.isEmpty()) {
-            throw new FileEmptyException();
-        }
-
-        for (MultipartFile file : files) {
-            if (!fileUtils.isValidMimeType(file)) {
-                throw new FileUnsupportedFormatException();
-            }
-
-            log.info("fileName : {}\n", file.getOriginalFilename());
-        }
-
-        return fileAdaptor.communicateWithAiServer(files).toString();
-    }
-
+public interface FileService {
+    String communicateWithAiServer(List<MultipartFile> files);
 }
