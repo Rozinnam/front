2739321689cff2a6b86e16b67fb06b475ca3f514
Damho Lee <ydh4745@gmail.com>
2025-04-11 17:12:10 +0900

    [feat] WebSocket 방식 삭제. Sse 방식으로 교체

--- src/main/java/com/example/front/webhook/controller/RestWebhookController.java
+++ src/main/java/com/example/front/webhook/controller/RestWebhookController.java
@@ -1,11 +1,11 @@
 package com.example.front.webhook.controller;
 
 import com.example.front.file.dto.response.ResponseDto;
+import com.example.front.sse.SseRestController;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.http.ResponseEntity;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/webhook")
 @RequiredArgsConstructor
 public class RestWebhookController {
-    private final SimpMessagingTemplate messagingTemplate;
+    private final SseRestController sseRestController;
 
     @PostMapping("/result")
     public ResponseEntity<String> receiveResult(@RequestBody ResponseDto result) {
@@ -28,9 +28,8 @@ public class RestWebhookController {
             return ResponseEntity.badRequest().body("TaskId가 유효하지 않습니다.");
         }
 
-        messagingTemplate.convertAndSend("/topic/result/" + taskId, result.toString());
-        log.info("TaskId : {}\n", taskId);
-        log.info("result : {}\n", result);
+        sseRestController.sendResult(taskId, result.toString());
+        log.info("결과 전송 완료: {}", result);
 
         return ResponseEntity.ok("결과 전송 완료");
     }
