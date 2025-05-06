2551f2659071113c4fbfae9308c847c1e3f6585a
Damho Lee <ydh4745@gmail.com>
2025-05-06 16:15:44 +0900

    [feat] 매일 00시마다 Redis 에 저장된 viewcount 값 DB 로 적재 후 삭제

---
+++ src/main/java/com/example/front/pageview/scheduler/PageViewCountScheduler.java
@@ -0,0 +1,34 @@
+package com.example.front.pageview.scheduler;
+
+import com.example.front.pageview.entity.PageType;
+import com.example.front.pageview.repository.PageViewCountRepository;
+import com.example.front.pageview.service.PageViewCountService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+public class PageViewCountScheduler {
+    private final PageViewCountService pageViewCountService;
+    private final PageViewCountRepository pageViewCountRepository;
+
+    @Scheduled(cron = "0 0 0 * * *")
+    @Transactional
+    public void syncViewCountToDB() {
+        Map<PageType, Long> viewCounts = pageViewCountService.getAllViewCounts();
+        LocalDate date = LocalDate.now().minusDays(1);
+
+        for (Map.Entry<PageType, Long> entry : viewCounts.entrySet()) {
+            PageType pageType = entry.getKey();
+            Long count = entry.getValue();
+
+            pageViewCountRepository.saveOrUpdate(date, pageType, count);
+            pageViewCountService.deleteViewCount(pageType);
+        }
+    }
+}
