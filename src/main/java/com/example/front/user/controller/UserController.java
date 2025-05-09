ceb52d632269c0203eb67969fcdc68162921a6f8
Damho Lee <ydh4745@gmail.com>
2025-05-09 15:04:09 +0900

    [feat] 로그 삭제

--- src/main/java/com/example/front/user/controller/UserController.java
+++ src/main/java/com/example/front/user/controller/UserController.java
@@ -18,7 +18,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Map;
 
-@Slf4j
 @Controller
 @RequestMapping("/admin")
 @RequiredArgsConstructor
@@ -28,24 +27,20 @@ public class UserController {
 
     @GetMapping("/login")
     public String getLoginPage() {
-        log.info("getLoginPage()");
         return "admin/login";
     }
 
     @GetMapping("/logout")
     public String logout(HttpSession session) {
         session.invalidate();
-        log.info("logout()");
 
         return "redirect:/admin";
     }
 
     @GetMapping("")
     public String getAdminPage(HttpSession session, Model model) {
-        log.info("getAdminPage()");
         User user = (User) session.getAttribute("loginUser");
         if (user == null || UserRole.ADMIN != user.getRole()) {
-            log.info("redirect:/admin/login");
             return "redirect:/admin/login";
         }
 
@@ -57,20 +52,17 @@ public class UserController {
 
     @PostMapping("/login")
     public String requestLogin(@RequestParam String id, @RequestParam String password, HttpSession session, HttpServletRequest request) {
-        log.info("requestLogin()");
         session.invalidate();
         session = request.getSession(true);
 
         User user = userService.login(id, password);
         if (user == null) {
-            log.info("redirect:/admin/login");
             return "redirect:/admin/login";
         }
 
         session.setAttribute("loginUser", user);
         session.setMaxInactiveInterval(600);
 
-        log.info("redirect:/admin");
         return "redirect:/admin";
     }
 }
