61a8c446d01398c9b24ca88d6f85209632c2b326
damho-lee <ydh4745@gmail.com>
2025-09-12 13:47:34 +0900

    package 명 수정

--- src/main/java/com/ggiiig/file/service/DelegatingFileService.java
+++ src/main/java/com/ggiiig/file/service/DelegatingFileService.java
@@ -1,7 +1,7 @@
-package com.example.front.file.service;
+package com.ggiiig.file.service;
 
 import com.ggiiig.featureflag.FeatureFlagService;
-import com.example.front.part.domain.CarPart;
+import com.ggiiig.part.domain.CarPart;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
