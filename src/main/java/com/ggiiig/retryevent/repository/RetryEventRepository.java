9106480f1b461e8506fa0bb28d2a17cef57b6b08
damho-lee <ydh4745@gmail.com>
2025-09-29 20:51:14 +0900

    import optimize

--- src/main/java/com/ggiiig/retryevent/repository/RetryEventRepository.java
+++ src/main/java/com/ggiiig/retryevent/repository/RetryEventRepository.java
@@ -3,7 +3,6 @@ package com.ggiiig.retryevent.repository;
 import com.ggiiig.retryevent.entity.RetryEvent;
 import com.ggiiig.retryevent.entity.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Component;
 
 import java.util.List;
 
